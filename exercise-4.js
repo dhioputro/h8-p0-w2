8a4406be872c9b8e8fc6c36e9d667c89cb86b371
dhioputro <dhiopalem@gmail.com>
2019-12-17 22:47:36 +0700

    update exercise-4

--- exercise-4.js
+++ exercise-4.js
@@ -1,31 +1,45 @@
 // var hari = 21; var bulan = 1; var tahun = 1945;
 //Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
 
-var bulan = 6;
+var tanggal  = 12;
+var bulan = 5;
+var tahun =  1992;
+
 switch(bulan) {
   case 1  : 
-    console.log ("21 Januari 1945");
+    console.log (tanggal + " January " + tahun);
     break;
   case 2  : 
-    console.log ("21 Februari 1945"); 
+    console.log (tanggal + " February " + tahun);
     break;
   case 3  : 
-    console.log ("21 Maret 1945"); 
+    console.log (tanggal + " Maret " + tahun);
     break; 
   case 4  : 
-    console.log ("21 April 1945"); 
+    console.log (tanggal + " April " + tahun);
     break; 
   case 5  : 
-    console.log ("21 Mei 1945"); 
+    console.log (tanggal + " May " + tahun);
     break;  
   case 6  : 
-    console.log ("21 Juni 1945"); 
+    console.log (tanggal + " June " + tahun);
     break; 
   case 7  : 
-    console.log ("21 Juli 1945"); 
+    console.log (tanggal + " July " + tahun);
     break; 
-  default : 
-    console.log ("tidak ada data"); 
+  case 8  : 
+    console.log (tanggal + " August " + tahun);
     break;
-  
+  case 9  : 
+    console.log (tanggal + " September " + tahun);
+    break;
+  case 10  : 
+    console.log (tanggal + " October " + tahun);
+    break;
+  case 11  : 
+    console.log (tanggal + " November " + tahun);
+    break;
+  case 12  : 
+    console.log (tanggal + " December " + tahun);
+    break;
 }
